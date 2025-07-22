2919e069bedabb2d2cbd6088897932710d6369d7
urooyo <inertia_mailer_0c@icloud.com>
2025-07-22 12:59:34 +0900

    feat: 아이콘 모듈 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'unplugin-icons/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', 'unplugin-icons/nuxt', '@nuxt/icon'],
   css: ['~/assets/css/font.css', '~/assets/css/main.css'],
 })

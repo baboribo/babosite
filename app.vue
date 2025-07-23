cd199d574d81b0c7e4c530c4c0a0aa2665d11a5b
baboribo <inertia_mailer_0c@icloud.com>
2025-07-23 22:09:43 +0900

    refactor: 프로필 정보 및 링크는 전부 v-for를 사용하도록 변경. style: 모바일과 태블릿을 위한 반응형 최적화 적용.

--- app.vue
+++ app.vue
@@ -1,15 +1,15 @@
 <script setup>
   useSeoMeta({
-    title: '바보리보',
-    description: '바보리보의 사이트입니다!',
+    title: '바보리보 (@baboribo)',
+    description: '...',
     ogTitle: '바보리보',
-    ogDescription: '바보리보의 사이트입니다!',
+    ogDescription: '...',
     ogUrl: 'https://baboribo.yume.place',
-    ogImage: '/d80eac4321e1b29abfbef07fa38d660c-removebg-preview.png',
+    ogImage: 'https://avatars.githubusercontent.com/u/71020988?v=4',
     twitterTitle: '바보리보',
-    twitterDescription: '바보리보의 사이트입니다!',
+    twitterDescription: '...',
     twitterCard: 'summary',
-    twitterImage: '/d80eac4321e1b29abfbef07fa38d660c-removebg-preview.png'
+    twitterImage: 'https://avatars.githubusercontent.com/u/71020988?v=4'
   })
 
   useHead({
@@ -28,6 +28,7 @@
   import { ref } from 'vue'
   import './assets/css/main.css'
   import './assets/css/font.css'
+  import { profileme } from './assets/info/my-profile.ts'
   import { langname } from './assets/info/langname.ts'
   import { links } from './assets/info/links.ts'
   import modal from './components/modal.vue'
@@ -36,21 +37,24 @@
 </script>
 <template>
   <modal v-if="isModalOpen" @close="isModalOpen = false" />
-  <div id="app" class="max-w-6xl mx-auto">
-    <header class="flex col-span-2 w-full"> <!-- 프로필 그룹((좌)프로필 정보, (우)자기소개, 관심사, 등) -->
+  <div id="app" class="max-w-5xl mx-auto">
+    <header v-for="(profile) in profileme" :key="profile" class="flex flex-col col-span-2 lg:flex-row"> <!-- 프로필 그룹((좌)프로필 정보, (우)자기소개, 관심사, 등) -->
       <div class="flex flex-col w-full"> <!-- 프로필과 언어별 표시이름이 있는 곳 -->
         <section class="flex items-center gap-4"> <!-- 프로필 정보 -->
           <button @click="isModalOpen = true">
-            <img class="avatar" src="./public/d80eac4321e1b29abfbef07fa38d660c-removebg-preview.png" alt="Avatar"/>
+            <img class="avatar" :src="profile.avatar" alt="Avatar"/>
           </button>
           <div class="flex flex-col pb-1 gap-0.5"> <!-- 프로필 정보 내부 -->
             <div class="flex items-center gap-2">
-              <h2>바보리보</h2>
-              <p>@baboribo</p>
+              <h2>{{ profile.displayname }}</h2>
+              <div class="flex items-center">
+                <p>@</p>
+                <p>{{ profile.username }}</p>
+              </div>
             </div>
-            <div class="flex items-center">
+            <div  class="flex items-center">
               <p>"</p>
-              <p id="self-introduction">일단 뭐든지 질투하는 사람.</p>
+              <p>{{ profile.MeInOneWord }}</p>
               <p>"</p>
             </div>
           </div>
@@ -70,18 +74,15 @@
           </div> <!-- 언어별 표시이름 그룹 [끝지점] -->
         </section> <!-- [프레임] 언어별 표시이름 그룹과 자기소개, 관심사 그룹 [끝지점] -->
       </div> <!-- 프로필과 언어별 표시이름이 있는 곳 [끝지점] -->
-      <div class="flex flex-col w-full"> <!-- 자기소개와 관심사 그룹 -->
-        <section class="flex flex-col w-full gap-1"> <!-- 자기소개 -->
+      <div class="isme-group"> <!-- 자기소개와 관심사 그룹 -->
+        <section class="flex flex-col w-full gap-1 pr-3"> <!-- 자기소개 -->
           <h4 class="group-h4">자기소개</h4>
-          <p>안녕하세요! 저는 바보리보라고 해요. 앞 날이 걱정됩니다..!</p>
+          <p>{{ profile.about }}</p>
         </section> <!-- 자기소개 [끝지점] -->
-        <section class="flex flex-col gap-1"> <!-- 관심사 -->
+        <section class="flex flex-col gap-1 w-full"> <!-- 관심사 -->
           <h4 class="group-h4">관심사</h4>
-          <ul class="flex gap-2">
-            <li class="tag">음악</li>
-            <li class="tag">그림</li>
-            <li class="tag">웹 디자인</li>
-            <li class="tag">웹 개발</li>
+          <ul class="flex flex-row gap-2">
+            <li v-for="(interest, i) in profile.interests" :key="i" class="tag">{{ interest.name }}</li>
           </ul>
         </section> <!-- 관심사 [끝지점] -->
       </div> <!-- 자기소개와 관심사 그룹 [끝지점] -->
@@ -93,9 +94,9 @@
           <a class="link-card-ui" :href="link.url" target="_blank" rel="noopener noreferrer">
             <div class="link-card-content">
               <component :is="link.icon" size="24" />
-              <div class="flex items-center gap-4">
+              <div class="flex flex-col sm:flex-row sm:gap-4 justify-start">
                 <h4>{{ link.name }}</h4>
-                <p>{{ link.description }}</p>
+                <p class="mt-1">{{ link.description }}</p>
               </div>
             </div>
             <Icon name="mingcute:arrow-right-up-line" style="color: #E4E3ED" size="24" />
--- assets/info/links.ts
+++ assets/info/links.ts
@@ -1,16 +1,28 @@
-import { GitHubIcon, RobloxIcon } from 'vue3-simple-icons'
+import { GitHubIcon, RobloxIcon, SoundCloudIcon, OsuIcon } from 'vue3-simple-icons'
 
 export const links = [
   {
-    name: 'GitHub 프로필',
+    name: 'GitHub',
     url: 'https://github.com/baboribo',
     icon: GitHubIcon,
-    description: '개발 관련 정보와 소스 코드를 확인할 수 있습니다.'
+    description: '제 프로필과 리포지토리를 볼 수 있어요!'
   },
   {
-    name: 'Roblox 프로필',
+    name: 'SoundCloud',
+    url: 'https://soundcloud.com/baboribo',
+    icon: SoundCloudIcon,
+    description: '그냥 주로 음악 듣고 가끔씩 이상한 걸 올려요.'
+  },
+  {
+    name: 'Roblox',
     url: 'https://www.roblox.com/users/244515714/profile',
     icon: RobloxIcon,
-    description: '내가 주로 하는 게임!'
-  }
+    description: '어릴 때부터 많이 했고, 지금은 가끔씩 해요.'
+  },
+  {
+    name: '내 osu!',
+    url: 'https://osu.ppy.sh/users/35965705',
+    icon: OsuIcon,
+    description: 'osu! 프로필이에요. 기본은 maina입니다.'
+  },
 ]
